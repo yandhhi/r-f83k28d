1f170c00eff5d6d607095f19fc76cc9354d19874
Andres Higuita <yecid3257@live.com>
2021-04-01 22:01:59 -0500

    resolve issue

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      count: 0,
+    };
+  }
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.count}</span>
+        <button id="inc" onClick={this.increase.bind(this)}>
+          Incrementa
+        </button>
       </div>
     );
+  }
+  increase() {
+    this.setState({ count: this.state.count + 1 });
   }
 }
 
